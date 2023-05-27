659a88f918e488deb835f0cba831dcaf094468bd
JoseMurillas <jmurillas99@gmail.com>
2023-05-27 15:50:20 -0500

    nuevos cambios

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-function calcularPropina() {
+//Funcion nos ayuda ocultar el inicio
+window.onload = function() {
+  mostrarSaludo();
+};
+function mostrarSaludo() {
+  document.getElementById('principal').style.display = 'block';
+  document.getElementById('calculadora_tip').style.display = 'none';
+  var indicator = document.getElementById('indicator'); //Agregamos color cuando el usuario presione inicio
+  for (var i = 0; i < indicators.length; i++) {
+    indicators[i].style.backgroundColor = 'gray';
+  }
+}
+
+function mostrarCalculadora() {
+  document.getElementById('principal').style.display = 'none';
+  document.getElementById('calculadora_tip').style.display = 'block';
+  var indicator = document.getElementById('indicator');//Agregamos color cuando el usuario presione calculadora de propinas
+  for (var i = 0; i < indicators.length; i++) {
+    indicators[i].style.backgroundColor = 'gray';
+  }
+}
+
+function calcularPropina() { //Creamos las funciones de la calculadora de propinas
   var total = parseFloat(document.getElementById("total").value);
   var percentage = parseFloat(document.getElementById("percentage").value);
   
@@ -8,7 +30,7 @@ function calcularPropina() {
   }
   
   var tip = total * (percentage / 100);
-  var totalAmount = total + tip;
+  var totalAmount = (total + tip)-total;
   
   document.getElementById("totalAmount").textContent = "$" + totalAmount.toFixed(2);
 }
